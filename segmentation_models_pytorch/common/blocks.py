66816b4eaca51340db0d755e7e31c89863d7e459
qubvel <qubvel@gmail.com>
2019-03-06 19:09:46 +0300

    Sequential block

--- segmentation_models_pytorch/common/blocks.py
+++ segmentation_models_pytorch/common/blocks.py
@@ -6,16 +6,17 @@ class Conv2dReLU(nn.Module):
                  stride=1, use_batchnorm=True, **batchnorm_params):
 
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size,
-                              stride=stride, padding=padding, bias=not (use_batchnorm))
-        if use_batchnorm:
-            self.batchnorm = nn.BatchNorm2d(out_channels, **batchnorm_params)
 
-        self.activation = nn.ReLU(inplace=True)
+        layers = [
+            nn.Conv2d(in_channels, out_channels, kernel_size,
+                              stride=stride, padding=padding, bias=not (use_batchnorm)),
+            nn.ReLU(inplace=True),
+        ]
+
+        if use_batchnorm:
+            layers.insert(1, nn.BatchNorm2d(out_channels, **batchnorm_params))
+
+        self.block = nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv(x)
-        if hasattr(self, 'batchnorm'):
-            x = self.batchnorm(x)
-        x = self.activation(x)
-        return x
+        return self.block(x)
