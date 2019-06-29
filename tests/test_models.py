474f922874b27f339e7e3f8e444eff3c511a83e7
Pavel Yakubovskiy <qubvel@gmail.com>
2019-06-30 01:47:44 +0300

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,7 +6,7 @@ import torch
 import random
 
 # mock detection module 
-sys.modules['torchvision.detection'] = mock.Mock()
+sys.modules['torchvision._C'] = mock.Mock()
 
 import segmentation_models_pytorch as smp
 
