bbb8ae8f4c073c122ea00ded13937f39850eb1fa
qubvel <qubvel@gmail.com>
2019-09-18 19:33:26 +0300

    refactor losses and metrics

--- segmentation_models_pytorch/utils/losses.py
+++ segmentation_models_pytorch/utils/losses.py
@@ -5,53 +5,63 @@ from . import functions as F
 from .. import common as cmn
 
 
+class JaccardLoss(base.Loss):
 
-
-
-class JaccardLoss(base.Named):
-
-    def __init__(self, eps=1e-7, activation=None, **kwargs):
+    def __init__(self, eps=1e-7, activation=None, ignore_channels=None, **kwargs):
         super().__init__(**kwargs)
         self.eps = eps
         self.activation = cmn.Activation(activation, dim=1)
+        self.ignore_channels = ignore_channels
 
     def forward(self, y_pr, y_gt):
         y_pr = self.activation(y_pr)
-        return 1 - F.jaccard(y_pr, y_gt, eps=self.eps, threshold=None)
+        return 1 - F.jaccard(
+            y_pr, y_gt,
+            eps=self.eps,
+            threshold=None,
+            ignore_channels=self.ignore_channels,
+        )
 
 
-class DiceLoss(base.Named):
+class DiceLoss(base.Loss):
 
-    def __init__(self, eps=1e-7, activation=None, **kwargs):
+    def __init__(self, eps=1e-7, beta=1., activation=None, ignore_channels=None, **kwargs):
         super().__init__(**kwargs)
         self.eps = eps
+        self.beta = beta
         self.activation = cmn.Activation(activation, dim=1)
+        self.ignore_channels = ignore_channels
 
     def forward(self, y_pr, y_gt):
         y_pr = self.activation(y_pr)
-        return 1 - F.f_score(y_pr, y_gt, beta=1., eps=self.eps, threshold=None)
+        return 1 - F.f_score(
+            y_pr, y_gt,
+            beta=self.beta,
+            eps=self.eps,
+            threshold=None,
+            ignore_channels=self.ignore_channels,
+        )
 
 
-class BCEJaccardLoss(JaccardLoss):
-
-    def __init__(self, eps=1e-7, activation=None, logits=True, **kwargs):
-        super().__init__(eps, activation, **kwargs)
-        self.bce = nn.BCEWithLogitsLoss(reduction='mean') if logits else nn.BCELoss(reduction='mean')
-        self.activation = cmn.Activation(activation)
-
-    def forward(self, y_pr, y_gt):
-        jaccard = super().forward(y_pr, y_gt)
-        bce = self.bce(y_pr, y_gt)
-        return jaccard + bce
+class L1Loss(nn.L1Loss, base.Loss):
+    pass
 
 
-class BCEDiceLoss(DiceLoss):
+class MSELoss(nn.MSELoss, base.Loss):
+    pass
 
-    def __init__(self, eps=1e-7, activation=None, logits=True, **kwargs):
-        super().__init__(eps, activation, **kwargs)
-        self.bce = nn.BCEWithLogitsLoss(reduction='mean') if logits else nn.BCELoss(reduction='mean')
 
-    def forward(self, y_pr, y_gt):
-        dice = super().forward(y_pr, y_gt)
-        bce = self.bce(y_pr, y_gt)
-        return dice + bce
+class CrossEntropyLoss(nn.CrossEntropyLoss, base.Loss):
+    pass
+
+
+class NLLLoss(nn.NLLLoss, base.Loss):
+    pass
+
+
+class BCELoss(nn.BCELoss, base.Loss):
+    pass
+
+
+class BCEWithLogitsLoss(nn.BCEWithLogitsLoss, base.Loss):
+    pass
