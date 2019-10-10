b83e00088c0f8af024531206d2ccc2f3c96a8fb4
Siarhei Fiedartsou <siarhei.fedartsou@gmail.com>
2019-10-10 18:10:22 +0300

    Add ability to use Activated BatchNorm in decoder. (#81)

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,7 @@ import mock
 import pytest
 import torch
 import random
+import importlib
 
 # mock detection module 
 sys.modules['torchvision._C'] = mock.Mock()
@@ -35,9 +36,8 @@ def _select_names(names, k=2):
         return names
 
 
-def _test_forward_backward(model_fn, encoder_name):
-
-    model = model_fn(encoder_name, encoder_weights=None)
+def _test_forward_backward(model_fn, encoder_name, **model_params):
+    model = model_fn(encoder_name, encoder_weights=None, **model_params)
 
     x = torch.ones((1, 3, 64, 64))
     y = model.forward(x)
@@ -45,8 +45,8 @@ def _test_forward_backward(model_fn, encoder_name):
     l.backward()
 
 
-def _test_pretrained_model(model_fn, encoder_name, encoder_weights):
-    model = model_fn(encoder_name, encoder_weights=encoder_weights)
+def _test_pretrained_model(model_fn, encoder_name, encoder_weights, **model_params):
+    model = model_fn(encoder_name, encoder_weights=encoder_weights, **model_params)
 
     x = torch.ones((1, 3, 64, 64))
     y = model.predict(x)
@@ -80,6 +80,12 @@ def test_linknet(encoder_name):
 def test_pspnet(encoder_name):
     _test_forward_backward(smp.PSPNet, encoder_name)
     _test_pretrained_model(smp.PSPNet, encoder_name, get_pretrained_weights_name(encoder_name))
+
+
+@pytest.mark.skipif(importlib.util.find_spec('inplace_abn') is None, reason='')
+def test_inplace_abn():
+    _test_forward_backward(smp.Unet, 'resnet18', decoder_use_batchnorm='inplace')
+    _test_pretrained_model(smp.Unet, 'resnet18', get_pretrained_weights_name('resnet18'), decoder_use_batchnorm='inplace')
 
 
 if __name__ == '__main__':
