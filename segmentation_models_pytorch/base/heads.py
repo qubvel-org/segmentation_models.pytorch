8d66a863ca6f3d47187b4ea2d44ef9355d601725
Pavel Yakubovskiy <qubvel@gmail.com>
2019-11-06 18:22:24 +0300

    Add AdaptiveMaxPool option for classification head

--- segmentation_models_pytorch/base/heads.py
+++ segmentation_models_pytorch/base/heads.py
@@ -13,8 +13,10 @@ class SegmentationHead(nn.Sequential):
 
 class ClassificationHead(nn.Sequential):
 
-    def __init__(self, in_channels, classes, dropout=0.2, activation=None):
-        pool = nn.AdaptiveAvgPool2d(output_size=1)
+    def __init__(self, in_channels, classes, pooling="avg", dropout=0.2, activation=None):
+        if pooling not in ("max", "avg"):
+            raise ValueError("Pooling should be one of ('max', 'avg'), got {}.".format(pooling))
+        pool = nn.AdaptiveAvgPool2d(1) if pooling == 'avg' else nn.AdaptiveMaxPool2d(1)
         flatten = Flatten()
         dropout = nn.Dropout(p=dropout, inplace=True) if dropout else nn.Identity()
         linear = nn.Linear(in_channels, classes, bias=True)
