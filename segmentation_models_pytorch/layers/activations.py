476f584cc16c95a24391b1f2464fd5e9688b6287
Yuval Spiegel <yuvalspiegel1@gmail.com>
2019-11-23 15:43:55 +0200

    adding layers, changing model.py and decoders

---
+++ segmentation_models_pytorch/layers/activations.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+
+
+activations = {'sigmoid': torch.nn.Sigmoid, 'softmax2d': torch.nn.Softmax2d, None: 'linear'}
+
+
+class ActivationMaker(nn.Module):
+    def __init__(self, activation_name):
+        super().__init__()
+        self.activation_name = activation_name
+
+    def forward(self, x):
+        if self.activation_name:
+            activation_fn = activations[self.activation_name]()
+            x = activation_fn(x)
+        return x
+
+
+class ThresholdTaker(nn.Module):
+    def __init__(self, threshold):
+        super().__init__()
+        self.threshold = threshold
+
+    def forward(self, x):
+        if self.threshold:
+            x = (x > self.threshold).float()
+        return x
+
+
+class ArgmaxTaker(nn.Module):
+    """
+    input: size (b, c, h, w)
+    output: size (b, 1, h, w)
+    """
+    def __init__(self, take_argmax_flag):
+        super().__init__()
+        self.take_argmax_flag = take_argmax_flag
+
+    def forward(self, x):
+        if self.take_argmax_flag:
+            x = torch.argmax(x, dim=1).unsqueeze(1).type(torch.cuda.FloatTensor)
+        return x
+
+
+
---
+++ segmentation_models_pytorch/layers/out_modules.py
@@ -0,0 +1,51 @@
+from segmentation_models_pytorch.base.model import NetModule
+from segmentation_models_pytorch.layers import activations
+
+
+class OutLayer(NetModule):
+    def __init__(self):
+        super().__init__()
+
+
+class OutSigmoid(OutLayer):
+    def __init__(self, threshold=0.5):
+        super().__init__()
+        self.activation_maker = activations.ActivationMaker('sigmoid')
+        self.threshold_taker = activations.ThresholdTaker(threshold)
+
+    def forward(self, x):
+        x = self.activation_maker(x)
+        x = self.threshold_taker(x)
+        return x
+
+
+class OutSoftmax(OutLayer):
+    def __init__(self, take_argmax_flag=True):
+        super().__init__()
+        self.activation_maker = activations.ActivationMaker('softmax2d')
+        self.argmax_taker = activations.ArgmaxTaker(take_argmax_flag)
+
+    def forward(self, x):
+        x = self.activation_maker(x)
+        x = self.argmax_taker(x)
+        return x
+
+
+class PredictionLayer(OutLayer):
+    def __init__(self, activation_name):
+        super().__init__()
+        if activation_name == 'sigmoid':
+            self.layer = OutSigmoid()
+        elif activation_name == 'softmax2d':
+            self.layer = OutSoftmax()
+        else:
+            self.layer = None
+
+    def forward(self, x):
+        if self.layer:
+            x = self.layer(x)
+        return x
+
+
+
+
