4895ccf07ab1de8fd0236c8a5846fef430d48d60
Ilya Dobrynin <iidobryn@mts.ru>
2019-12-18 16:32:40 +0300

    [feat]: update PAN

--- segmentation_models_pytorch/pan/model.py
+++ segmentation_models_pytorch/pan/model.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union, List
+from typing import Optional, Union
 from .decoder import PANDecoder
 from ..encoders import get_encoder
 from ..base import SegmentationModel
@@ -6,7 +6,7 @@ from ..base import SegmentationHead, ClassificationHead
 
 
 class PAN(SegmentationModel):
-    """ Unet_ is a fully convolution neural network for image semantic segmentation
+    """ Implementation of _PAN (Pyramid Attention Network)
 
     Args:
         encoder_name: name of classification model (without last dense layers) used as feature
@@ -16,16 +16,15 @@ class PAN(SegmentationModel):
             [(H,W), (H/2, W/2), (H/4, W/4), (H/8, W/8)], so in general the deepest feature tensor will have
             spatial resolution (H/(2^depth), W/(2^depth)]
         encoder_weights: one of ``None`` (random initialization), ``imagenet`` (pre-training on ImageNet).
-        decoder_channels: list of numbers of ``Conv2D`` layer filters in decoder blocks
-        decoder_use_batchnorm: if ``True``, ``BatchNormalisation`` layer between ``Conv2D`` and ``Activation`` layers
-            is used. If 'inplace' InplaceABN will be used, allows to decrease memory consumption.
-            One of [True, False, 'inplace']
-        decoder_attention_type: attention module used in decoder of the model
-            One of [``None``, ``scse``]
+        decoder_channels: Number of ``Conv2D`` layer filters in decoder blocks
+        gau_upscale_mode: Upscale mode for decoder;
+            One of [``nearest``, ``'inear``, ``bilinear``, ``bicubic``, ``trilinear``, ``area``]. Default: ``bilinear``
         in_channels: number of input channels for model, default is 3.
         classes: a number of classes for output (output shape - ``(batch, classes, h, w)``).
         activation: activation function to apply after final convolution;
             One of [``sigmoid``, ``softmax``, ``logsoftmax``, ``identity``, callable, None]
+        upsampling: optional, final upsampling factor
+            (default is 4 to preserve input -> output spatial shape identity)
         aux_params: if specified model will have additional classification auxiliary output
             build on top of encoder, supported params:
                 - classes (int): number of classes
@@ -34,29 +33,30 @@ class PAN(SegmentationModel):
                 - activation (str): activation function to apply "sigmoid"/"softmax" (could be None to return logits)
 
     Returns:
-        ``torch.nn.Module``: **Unet**
+        ``torch.nn.Module``: **PAN**
 
-    .. _Unet:
-        https://arxiv.org/pdf/1505.04597
+    .. _PAN:
+        https://arxiv.org/abs/1805.10180
 
     """
 
     def __init__(
-        self,
-        backbone: str = "resnet34",
-        encoder_depth: int = 5,
-        encoder_weights: str = "imagenet",
-        decoder_channels: int = 32,
-        in_channels: int = 3,
-        classes: int = 1,
-        activation: Optional[Union[str, callable]] = None,
-        aux_params: Optional[dict] = None
+            self,
+            encoder_name: str = "resnet34",
+            encoder_depth: int = 5,
+            encoder_weights: str = "imagenet",
+            decoder_channels: int = 32,
+            gau_upscale_mode: str = 'bilinear',
+            in_channels: int = 3,
+            classes: int = 1,
+            activation: Optional[Union[str, callable]] = None,
+            upsampling: int = 4,
+            aux_params: Optional[dict] = None
     ):
         super().__init__()
 
         self.encoder = get_encoder(
-            # encoder_name,
-            backbone,
+            encoder_name,
             in_channels=in_channels,
             depth=encoder_depth,
             weights=encoder_weights,
@@ -64,7 +64,8 @@ class PAN(SegmentationModel):
 
         self.decoder = PANDecoder(
             encoder_channels=self.encoder.out_channels,
-            decoder_channels=decoder_channels
+            decoder_channels=decoder_channels,
+            upscale_mode=gau_upscale_mode
         )
 
         self.segmentation_head = SegmentationHead(
@@ -72,7 +73,7 @@ class PAN(SegmentationModel):
             out_channels=classes,
             activation=activation,
             kernel_size=3,
-            upsampling=4
+            upsampling=upsampling
         )
 
         if aux_params is not None:
@@ -82,5 +83,5 @@ class PAN(SegmentationModel):
         else:
             self.classification_head = None
 
-        self.name = "u-{}".format(backbone)
+        self.name = "u-{}".format(encoder_name)
         self.initialize()
