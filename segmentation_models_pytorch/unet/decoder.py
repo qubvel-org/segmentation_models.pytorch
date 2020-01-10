d48923de949bd09d5ada0ecc7c0893c3eab474f4
rishabh-16 <akchitra99@gmail.com>
2020-01-10 14:22:48 +0530

    updated

--- segmentation_models_pytorch/unet/decoder.py
+++ segmentation_models_pytorch/unet/decoder.py
@@ -103,7 +103,7 @@ class UnetDecoder(nn.Module):
             for in_ch, skip_ch, out_ch in zip(in_channels, skip_channels, out_channels)
         ]
         self.blocks = nn.ModuleList(blocks)
-        self.depthwise = nn.Conv2d(out_channels,out_channels, kernel_size=3, padding=1, groups=out_channels)
+        self.depthwise = nn.Conv2d(17,17, kernel_size=3, padding=1, groups=17)
 
     def forward(self, *features):
 
