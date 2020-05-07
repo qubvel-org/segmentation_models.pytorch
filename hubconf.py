5ebe6e75defd9960eb0da83fa13b46fcb6f4eca9
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2020-05-07 11:50:44 +0530

    hubconf absolute import

--- hubconf.py
+++ hubconf.py
@@ -4,9 +4,9 @@ dependencies = [
 ]
 
 
-from .unet import Unet
-from .linknet import Linknet
-from .fpn import FPN
-from .pspnet import PSPNet
-from .deeplabv3 import DeepLabV3, DeepLabV3Plus
-from .pan import PAN
+from segmentation_models_pytorch.unet import Unet
+from segmentation_models_pytorch.linknet import Linknet
+from segmentation_models_pytorch.fpn import FPN
+from segmentation_models_pytorch.pspnet import PSPNet
+from segmentation_models_pytorch.deeplabv3 import DeepLabV3, DeepLabV3Plus
+from segmentation_models_pytorch.pan import PAN
