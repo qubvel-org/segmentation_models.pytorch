2509da2a5a1c247f7e454731c1b677b4f93a85aa
Siavash Sakhavi <s.sakhavi@gmail.com>
2020-10-24 22:34:46 +0800

    Update timm_efficientnet.py

--- segmentation_models_pytorch/encoders/timm_efficientnet.py
+++ segmentation_models_pytorch/encoders/timm_efficientnet.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 
 from timm.models.efficientnet import EfficientNet
 from timm.models.efficientnet import decode_arch_def, round_channels, default_cfgs
+from timm.models.layers.activations import Swish
 
 from ._base import EncoderMixin
 
@@ -41,7 +42,7 @@ def get_efficientnet_kwargs(channel_multiplier=1.0, depth_multiplier=1.0):
         num_features=round_channels(1280, channel_multiplier, 8, None),
         stem_size=32,
         channel_multiplier=channel_multiplier,
-        act_layer="swish",
+        act_layer=Swish,
         norm_kwargs={},  # TODO: check
         drop_rate=0.2,
         drop_path_rate=0.2,
