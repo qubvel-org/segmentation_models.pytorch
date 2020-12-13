551f613ab2e2d6273ea0fb1642ad3852eb2475f9
qubvel <qubvel@gmail.com>
2020-12-13 13:57:46 +0300

    Add wrapper for configs

--- segmentation_models_pytorch/__init__.py
+++ segmentation_models_pytorch/__init__.py
@@ -10,3 +10,33 @@ from . import encoders
 from . import utils
 
 from .__version__ import __version__
+
+from typing import Optional
+import torch
+
+
+def create_model(
+    arch: str,
+    encoder_name: str,
+    encoder_weights: Optional[str],
+    in_channels: int,
+    classes: int,
+    **kwargs,
+) -> torch.nn.Module:
+    """Models wrapper. Allows to create any model just with parametes"""
+    
+    archs = [Unet, UnetPlusPlus, Linknet, FPN, PSPNet, DeepLabV3, DeepLabV3Plus, PAN]
+    archs_dict = [a.__class__.__name__.lower() for a in archs]
+    try:
+        model_class = archs_dict[arch.lower()]
+    except KeyError:
+        raise KeyError("Wrong architecture type `{}`. Avalibale options are: {}".format(
+            arch, list(archs_dict.keys()),
+        ))
+    return model_class(
+        encoder_name=encoder_name,
+        encoder_weights=encoder_weights,
+        in_channels=in_channels,
+        classes=classes,
+        **kwargs,
+    )
