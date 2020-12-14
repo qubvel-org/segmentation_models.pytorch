7d86e97dbf38e69d3aebd839de59676df5b06257
Sergey Kolchenko <kolchenkosergey@gmail.com>
2020-12-14 12:31:11 -0600

    add MAnet arch

--- segmentation_models_pytorch/__init__.py
+++ segmentation_models_pytorch/__init__.py
@@ -1,5 +1,6 @@
 from .unet import Unet
 from .unetplusplus import UnetPlusPlus
+from .manet import MAnet
 from .linknet import Linknet
 from .fpn import FPN
 from .pspnet import PSPNet
@@ -10,35 +11,3 @@ from . import encoders
 from . import utils
 
 from .__version__ import __version__
-
-from typing import Optional
-import torch
-
-
-def create_model(
-    arch: str,
-    encoder_name: str = "resnet34",
-    encoder_weights: Optional[str] = "imagenet",
-    in_channels: int = 3,
-    classes: int = 1,
-    **kwargs,
-) -> torch.nn.Module:
-    """Models wrapper. Allows to create any model just with parametes
-    
-    """
-    
-    archs = [Unet, UnetPlusPlus, Linknet, FPN, PSPNet, DeepLabV3, DeepLabV3Plus, PAN]
-    archs_dict = {a.__name__.lower(): a for a in archs}
-    try:
-        model_class = archs_dict[arch.lower()]
-    except KeyError:
-        raise KeyError("Wrong architecture type `{}`. Avalibale options are: {}".format(
-            arch, list(archs_dict.keys()),
-        ))
-    return model_class(
-        encoder_name=encoder_name,
-        encoder_weights=encoder_weights,
-        in_channels=in_channels,
-        classes=classes,
-        **kwargs,
-    )
