29117caa38ef2ad839f6d789750f4d58c809632d
qubvel <qubvel@gmail.com>
2020-12-17 12:14:39 +0300

    Add constants

--- segmentation_models_pytorch/losses/__init__.py
+++ segmentation_models_pytorch/losses/__init__.py
@@ -1,10 +1,8 @@
-from __future__ import absolute_import
+from ._constants import BINARY_MODE, MULTICLASS_MODE, MULTILABEL_MODE
 
+from .jaccard import JaccardLoss
 from .dice import DiceLoss
 from .focal import FocalLoss
-from .jaccard import JaccardLoss
 from .lovasz import LovaszLoss
 from .soft_bce import SoftBCEWithLogitsLoss
 from .soft_ce import SoftCrossEntropyLoss
-
-from ._constants import BINARY_MODE, MULTICLASS_MODE, MULTILABEL_MODE
