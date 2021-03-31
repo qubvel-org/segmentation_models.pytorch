b78f91c38517dda02242eab36a666fe4cab37f2a
Julien Maille <julien.maille@gmail.com>
2021-03-31 13:19:52 +0200

    NEW: print timm-universal models in generate_table.py

--- misc/generate_table.py
+++ misc/generate_table.py
@@ -19,6 +19,22 @@ separator = "|".join(["-" * WIDTH] + [":" + "-" * (WIDTH - 2) + ":"] * (len(COLU
 print(wrap_row(header))
 print(wrap_row(separator))
 
+for encoder_name in smp.encoders.timm_universal_encoders:
+    try:
+        model = smp.encoders.TimmUniversalEncoder(encoder_name=encoder_name, in_channels=3, depth=5, pretrained=False)
+        params = sum(p.numel() for p in model.parameters())
+        if params // 1000000 > 5:
+            params = str(params // 1000000) + "M"
+        else:
+            params = str((params // 100000) / 10) + "M"
+        params = params.ljust(WIDTH, " ")
+    except:
+        params = "?".ljust(WIDTH, " ")
+    encoder_name = encoder_name.ljust(WIDTH, " ")
+    weights = "?".ljust(WIDTH, " ")
+    row = "|".join([encoder_name, weights, params])
+    print(wrap_row(row))
+
 for encoder_name, encoder in encoders.items():
     weights = "<br>".join(encoder["pretrained_settings"].keys())
     encoder_name = encoder_name.ljust(WIDTH, " ")
