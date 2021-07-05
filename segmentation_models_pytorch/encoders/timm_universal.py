348330dfb855cb23421b59ceb283431cf3632b3f
qubvel <qubvel@gmail.com>
2021-07-05 20:51:41 +0300

    fix output_stride not supported

--- segmentation_models_pytorch/encoders/timm_universal.py
+++ segmentation_models_pytorch/encoders/timm_universal.py
@@ -6,14 +6,19 @@ class TimmUniversalEncoder(nn.Module):
 
     def __init__(self, name, pretrained=True, in_channels=3, depth=5, output_stride=32): 
         super().__init__()
-        self.model = timm.create_model(
-            name,
+        kwargs = dict(
             in_chans=in_channels,
             features_only=True,
             output_stride=output_stride,
             pretrained=pretrained,
             out_indices=tuple(range(depth)),
         )
+
+        # not all models support output stride argument, drop it by default
+        if output_stride == 32:
+            kwargs.pop(output_stride)
+
+        self.model = timm.create_model(name, **kwargs)
 
         self._in_channels = in_channels
         self._out_channels = [3, ] + self.model.feature_info.channels()
