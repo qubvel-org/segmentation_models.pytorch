560e8ff860d7afba7b76a01a8566d2c85aab13d5
Yashika <40869641+yashika-git@users.noreply.github.com>
2022-07-29 13:31:29 +0530

    Update dice.py (#608)

--- segmentation_models_pytorch/losses/dice.py
+++ segmentation_models_pytorch/losses/dice.py
@@ -89,10 +89,10 @@ class DiceLoss(_Loss):
                 y_pred = y_pred * mask.unsqueeze(1)
 
                 y_true = F.one_hot((y_true * mask).to(torch.long), num_classes)  # N,H*W -> N,H*W, C
-                y_true = y_true.permute(0, 2, 1) * mask.unsqueeze(1)  # H, C, H*W
+                y_true = y_true.permute(0, 2, 1) * mask.unsqueeze(1)  # N, C, H*W
             else:
                 y_true = F.one_hot(y_true, num_classes)  # N,H*W -> N,H*W, C
-                y_true = y_true.permute(0, 2, 1)  # H, C, H*W
+                y_true = y_true.permute(0, 2, 1)  # N, C, H*W
 
         if self.mode == MULTILABEL_MODE:
             y_true = y_true.view(bs, num_classes, -1)
