285194593e302895c242c03816f82ee6f6da4969
Hans Gaiser <hansg91@gmail.com>
2022-10-13 17:05:27 +0200

    Make MobileNetV2 scriptable.

--- segmentation_models_pytorch/encoders/mobilenet.py
+++ segmentation_models_pytorch/encoders/mobilenet.py
@@ -35,32 +35,33 @@ class MobileNetV2Encoder(torchvision.models.MobileNetV2, EncoderMixin):
         self._depth = depth
         self._out_channels = out_channels
         self._in_channels = 3
+        self._stages = self.get_stages()
         del self.classifier
 
     def get_stages(self):
-        return [
+        return nn.ModuleList([
             nn.Identity(),
             self.features[:2],
             self.features[2:4],
             self.features[4:7],
             self.features[7:14],
             self.features[14:],
-        ]
+        ])
 
     def forward(self, x):
-        stages = self.get_stages()
-
         features = []
-        for i in range(self._depth + 1):
-            x = stages[i](x)
-            features.append(x)
+        for i, stage in enumerate(self._stages):
+            # Note: the somewhat strange format here with `i` is required for conversion to TorchScript.
+            if i <= self._depth:
+                x = stage(x)
+                features.append(x)
 
         return features
 
     def load_state_dict(self, state_dict, **kwargs):
         state_dict.pop("classifier.1.bias", None)
         state_dict.pop("classifier.1.weight", None)
-        super().load_state_dict(state_dict, **kwargs)
+        super().load_state_dict(state_dict, strict=False, **kwargs)
 
 
 mobilenet_encoders = {
@@ -76,7 +77,7 @@ mobilenet_encoders = {
             },
         },
         "params": {
-            "out_channels": (3, 16, 24, 32, 96, 1280),
+            "out_channels": [3, 16, 24, 32, 96, 1280],
         },
     },
 }
