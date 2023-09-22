ae0ace76825335bc38f7d0ee04abc1dc7a64d307
sherif-med <cherif.abedrazek@gmail.com>
2023-09-22 16:17:01 +0200

    updating encoder dict with effv2 encoders

--- segmentation_models_pytorch/encoders/__init__.py
+++ segmentation_models_pytorch/encoders/__init__.py
@@ -10,6 +10,7 @@ from .densenet import densenet_encoders
 from .inceptionresnetv2 import inceptionresnetv2_encoders
 from .inceptionv4 import inceptionv4_encoders
 from .efficientnet import efficient_net_encoders
+from .efficientnetv2 import efficientv2_net_encoders
 from .mobilenet import mobilenet_encoders
 from .xception import xception_encoders
 from .timm_efficientnet import timm_efficientnet_encoders
@@ -35,6 +36,7 @@ encoders.update(densenet_encoders)
 encoders.update(inceptionresnetv2_encoders)
 encoders.update(inceptionv4_encoders)
 encoders.update(efficient_net_encoders)
+encoders.update(efficientv2_net_encoders)
 encoders.update(mobilenet_encoders)
 encoders.update(xception_encoders)
 encoders.update(timm_efficientnet_encoders)
