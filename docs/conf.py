92089f115c496fdc47f2c824988f80433bc5d220
Pavel Iakubovskii <qubvel@gmail.com>
2024-05-31 08:09:56 +0000

    Fix

--- docs/conf.py
+++ docs/conf.py
@@ -67,8 +67,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # import karma_sphinx_theme
 # html_theme = "karma_sphinx_theme"
