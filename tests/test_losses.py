2c38b8019c41c33381f065a0053972efe7bff605
Aditi Mhatre <am.doc24@gmail.com>
2024-10-05 17:15:34 +0200

    Initialized Test for Focal Tversky Loss

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -92,6 +92,21 @@ def test_soft_tversky_score(y_true, y_pred, expected, eps, alpha, beta):
     actual = F.soft_tversky_score(y_pred, y_true, eps=eps, alpha=alpha, beta=beta)
     assert float(actual) == pytest.approx(expected, eps)
 
+@pytest.mark.parametrize(
+    ["y_true", "y_pred", "expected", "eps", "alpha", "beta", "gamma"],
+    [
+        [[1, 1, 1, 1], [1, 1, 1, 1], 1.0, 1e-5, 0.5, 0.5],
+        [[0, 1, 1, 0], [0, 1, 1, 0], 1.0, 1e-5, 0.5, 0.5],
+        [[1, 1, 1, 1], [1, 1, 0, 0], 2.0 / 3.0, 1e-5, 0.5, 0.5, 2.0], 
+    ],
+)
+
+def test_focal_tversky_score(y_true, y_pred, expected, eps, alpha, beta, gamma):
+    y_true = torch.tensor(y_true, dtype=torch.float32)
+    y_pred = torch.tensor(y_pred, dtype=torch.float32)
+    actual = F.soft_tversky_score(y_pred, y_true, eps=eps, alpha=alpha, beta=beta, gamma=gamma)
+    assert float(actual) == pytest.approx(expected, eps)
+
 
 @torch.no_grad()
 def test_dice_loss_binary():
