5089a7c141ced41078a48b22cec38a49d5029967
Ryan <23580140+brianhou0208@users.noreply.github.com>
2024-11-10 04:45:02 +0800

    add timm constructor arguments

--- segmentation_models_pytorch/decoders/segformer/model.py
+++ segmentation_models_pytorch/decoders/segformer/model.py
@@ -1,11 +1,12 @@
-from typing import Optional, Union
+from typing import Any, Optional, Union
 
-from segmentation_models_pytorch.encoders import get_encoder
 from segmentation_models_pytorch.base import (
-    SegmentationModel,
-    SegmentationHead,
     ClassificationHead,
+    SegmentationHead,
+    SegmentationModel,
 )
+from segmentation_models_pytorch.encoders import get_encoder
+
 from .decoder import SegformerDecoder
 
 
@@ -35,6 +36,7 @@ class Segformer(SegmentationModel):
                 - dropout (float): Dropout factor in [0, 1)
                 - activation (str): An activation function to apply "sigmoid"/"softmax"
                     (could be **None** to return logits)
+        kwargs: Arguments passed to the encoder class ``__init__()`` function. Applies only to ``timm`` models. Keys with ``None`` values are pruned before passing.
 
     Returns:
         ``torch.nn.Module``: **Segformer**
@@ -54,6 +56,7 @@ class Segformer(SegmentationModel):
         classes: int = 1,
         activation: Optional[Union[str, callable]] = None,
         aux_params: Optional[dict] = None,
+        **kwargs: dict[str, Any],
     ):
         super().__init__()
 
@@ -62,6 +65,7 @@ class Segformer(SegmentationModel):
             in_channels=in_channels,
             depth=encoder_depth,
             weights=encoder_weights,
+            **kwargs,
         )
 
         self.decoder = SegformerDecoder(
