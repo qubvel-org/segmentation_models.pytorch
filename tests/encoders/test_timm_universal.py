e8f7a4e16119f706ad9a43fb0b5676bf7b658ae5
Pavel Iakubovskii <qubvel@gmail.com>
2024-12-22 23:17:30 +0000

    Update encoders for timm-universal

--- tests/encoders/test_timm_universal.py
+++ tests/encoders/test_timm_universal.py
@@ -1,6 +1,9 @@
 from tests.encoders import base
-from tests.config import RUN_ALL_ENCODERS
 
 
 class TestTimmUniversalEncoder(base.BaseEncoderTester):
-    encoder_names = ["tu-resnet18"] if not RUN_ALL_ENCODERS else ["tu-resnet18"]
+    encoder_names = [
+        "tu-resnet18",  # for timm universal traditional-like encoder
+        "tu-convnext_atto",  # for timm universal transformer-like encoder
+        "tu-darknet17",  # for timm universal vgg-like encoder
+    ]
