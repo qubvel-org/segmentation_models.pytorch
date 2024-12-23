e1caeb9c9ad5bd78dd51000e146a7ea76df1cb70
Pavel Iakubovskii <qubvel@gmail.com>
2024-12-23 14:25:15 +0000

    [run-slow] Fixing run slow

--- tests/utils.py
+++ tests/utils.py
@@ -12,7 +12,6 @@ default_device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def get_commit_message():
     commit_msg = os.getenv("COMMIT_MESSAGE", "")
-    raise ValueError(commit_msg)
     return commit_msg.lower()
 
 
