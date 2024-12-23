23bfce6716df6d6eed3f470bf67d5d1aaae356a5
Pavel Iakubovskii <qubvel@gmail.com>
2024-12-23 12:15:07 +0000

    Remove custom encoder

--- tests/models/test_pan.py
+++ tests/models/test_pan.py
@@ -5,7 +5,6 @@ from tests.models import base
 @pytest.mark.pan
 class TestPanModel(base.BaseModelTester):
     test_model_type = "pan"
-    test_encoder_name = "resnet-18"
 
     default_batch_size = 2
     default_height = 128
