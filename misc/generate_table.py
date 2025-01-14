b2c13f1212cfd026b8f258afca59cad33904417d
Pavel Iakubovskii <qubvel@gmail.com>
2025-01-14 19:30:40 +0000

    Update encoders table

--- misc/generate_table.py
+++ misc/generate_table.py
@@ -1,11 +1,17 @@
+import os
 import segmentation_models_pytorch as smp
+
+from tqdm import tqdm
 
 encoders = smp.encoders.encoders
 
 
 WIDTH = 32
-COLUMNS = ["Encoder", "Weights", "Params, M"]
+COLUMNS = ["Encoder", "Pretrained weights", "Params, M", "Script", "Compile", "Export"]
+FILE = "encoders_table.md"
 
+if os.path.exists(FILE):
+    os.remove(FILE)
 
 def wrap_row(r):
     return "|{}|".format(r)
@@ -16,18 +22,24 @@ separator = "|".join(
     ["-" * WIDTH] + [":" + "-" * (WIDTH - 2) + ":"] * (len(COLUMNS) - 1)
 )
 
-print(wrap_row(header))
-print(wrap_row(separator))
+print(wrap_row(header), file=open(FILE, "a"))
+print(wrap_row(separator), file=open(FILE, "a"))
 
-for encoder_name, encoder in encoders.items():
+for encoder_name, encoder in tqdm(encoders.items()):
+
     weights = "<br>".join(encoder["pretrained_settings"].keys())
-    encoder_name = encoder_name.ljust(WIDTH, " ")
-    weights = weights.ljust(WIDTH, " ")
 
     model = encoder["encoder"](**encoder["params"], depth=5)
+
+    script = "✅" if model._is_torch_scriptable else "❌"
+    compile = "✅" if model._is_torch_compilable else "❌"
+    export = "✅" if model._is_torch_exportable else "❌"
+
     params = sum(p.numel() for p in model.parameters())
     params = str(params // 1000000) + "M"
-    params = params.ljust(WIDTH, " ")
 
-    row = "|".join([encoder_name, weights, params])
-    print(wrap_row(row))
+    row = [encoder_name, weights, params, script, compile, export]
+    row = [str(r).ljust(WIDTH, " ") for r in row]
+    row = "|".join(row)
+
+    print(wrap_row(row), file=open(FILE, "a"))
