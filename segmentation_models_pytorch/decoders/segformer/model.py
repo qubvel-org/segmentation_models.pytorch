d9ab82a70210b95955124093b5e2badb43ddcfa3
David Calhas <davidcalhas@hotmail.com>
2025-04-11 14:41:45 +0100

    add upsampling parameter #1106 (#1123)
    * add upsampling parameter #1106
    
    * docstring upsampling parameter #1123

--- segmentation_models_pytorch/decoders/segformer/model.py
+++ segmentation_models_pytorch/decoders/segformer/model.py
@@ -29,6 +29,7 @@ class Segformer(SegmentationModel):
         activation: An activation function to apply after the final convolution layer.
             Available options are **"sigmoid"**, **"softmax"**, **"logsoftmax"**, **"tanh"**, **"identity"**,
             **callable** and **None**. Default is **None**.
+        upsampling: A number to upsample the output of the model, default is 4 (same size as input)
         aux_params: Dictionary with parameters of the auxiliary output (classification head). Auxiliary output is build
             on top of encoder if **aux_params** is not **None** (default). Supported params:
                 - classes (int): A number of classes
@@ -56,6 +57,7 @@ class Segformer(SegmentationModel):
         in_channels: int = 3,
         classes: int = 1,
         activation: Optional[Union[str, Callable]] = None,
+        upsampling: int = 4,
         aux_params: Optional[dict] = None,
         **kwargs: dict[str, Any],
     ):
@@ -80,7 +82,7 @@ class Segformer(SegmentationModel):
             out_channels=classes,
             activation=activation,
             kernel_size=1,
-            upsampling=4,
+            upsampling=upsampling,
         )
 
         if aux_params is not None:
