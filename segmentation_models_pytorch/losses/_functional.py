c8f4a72a3fd6d05a1e0c75b86f2dc89a62bb195d
Lukas <info@lukassanner.de>
2025-07-22 11:16:52 +0200

    Fix/issue 1198 value error invalid type torch.mps.float tensor (#1199)
    * fixing type and device casting
    
    * updating version with fix
    
    * Revert "fixing type and device casting"
    
    This reverts commit 62682f045bccbda4054298dae8de60b74058d6a3.
    
    * Revert "updating version with fix"
    
    This reverts commit 0b431b0e3bfddfa52367e2e0f731d7c3766edca3.
    
    * fixing type and device casting
    
    * updating version with fix
    
    * Update segmentation_models_pytorch/__version__.py
    
    ---------
    
    Co-authored-by: Lukas Sanner <lukas.sanner@lgln.niedersachsen.de>
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- segmentation_models_pytorch/losses/_functional.py
+++ segmentation_models_pytorch/losses/_functional.py
@@ -66,7 +66,7 @@ def focal_loss_with_logits(
     References:
         https://github.com/open-mmlab/mmdetection/blob/master/mmdet/core/loss/losses.py
     """
-    target = target.type(output.type())
+    target = target.to(dtype=output.dtype, device=output.device)
 
     logpt = F.binary_cross_entropy_with_logits(output, target, reduction="none")
     pt = torch.exp(-logpt)
