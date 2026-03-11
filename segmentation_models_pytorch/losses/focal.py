c36a8b08fd74488cc56b575b4cc12bbbd71c7c53
Harsh Dahiya <148308520+Harsh-2005d@users.noreply.github.com>
2026-03-11 19:19:10 +0530

    Update segmentation_models_pytorch/losses/focal.py
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- segmentation_models_pytorch/losses/focal.py
+++ segmentation_models_pytorch/losses/focal.py
@@ -3,7 +3,7 @@ from functools import partial
 
 import torch
 from torch.nn.modules.loss import _Loss
-from ._functional import focal_loss_with_logits,softmax_focal_loss_with_logits
+from ._functional import focal_loss_with_logits, softmax_focal_loss_with_logits
 from .constants import BINARY_MODE, MULTICLASS_MODE, MULTILABEL_MODE
 
 __all__ = ["FocalLoss"]
